b5d694a6f4cbce6effef1f615001e875c618361b
amathael <dan.io.oreshnikov@gmail.com>
2021-03-14 02:03:25 +0300

    what the hell is this...

--- server/src/db/dao/game/Field.kt
+++ server/src/db/dao/game/Field.kt
@@ -1,7 +1,16 @@
 package lsd.wheel.db.dao.game
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 
-object FieldTable: IntIdTable() {
+object FieldTable : IntIdTable() {
     val name = varchar("name", 32).uniqueIndex()
+}
+
+class FieldEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<FieldEntity>(FieldTable)
+
+    val name by FieldTable.name
 }
--- server/src/service/data/game/Vertex.kt
+++ server/src/service/data/game/Vertex.kt
@@ -2,20 +2,10 @@ package lsd.wheel.service.data.game
 
 import lsd.wheel.game.Direction
 
-/**
- * created by imd on 13.03.2021
- */
-
-class Vertex(val id: Int) {
-    var east: Vertex? = null
-    var north: Vertex? = null
-    var west: Vertex? = null
-    var south: Vertex? = null
-
-    fun nextToDirection(dir: Direction) : Vertex? = when (dir) {
-        Direction.EAST -> east
-        Direction.NORTH -> north
-        Direction.WEST -> west
-        Direction.SOUTH -> south
-    }
-}
+data class Vertex(
+    val id: Int,
+    val x: Int,
+    val y: Int,
+    val items: MutableList<Item>,
+    val edges: MutableMap<Direction, Int>
+)
