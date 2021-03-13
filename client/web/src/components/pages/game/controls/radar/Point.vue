c15530e233d01f72e92142055a0502d9a4cfc68b
gcharkovskiy <g.charkovsky@gmail.com>
2021-03-13 21:50:29 +0300

    Начало радара

---
+++ client/web/src/components/pages/game/controls/radar/Point.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="className" :style="stylePosition">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Point",
+        props: {
+            current: Boolean,
+            active: Boolean,
+            position: {
+                x: Number,
+                y: Number,
+            },
+            description: Object,
+        },
+        computed: {
+            className() {
+                return "point" + (this.active ? " point-active" : "") + (this.current ? " point-current" : "")
+            },
+            stylePosition() {
+                return "top:" + this.position.x + "px; left:" + this.position.y + ";"
+            }
+        },
+        mounted() {
+            console.log(this);
+        }
+    }
+</script>
+
+<style scoped>
+    .point {
+        position: absolute;
+        background-color: #42b983;
+        border-radius: 1em;
+        width: 1em;
+        height: 1em;
+    }
+    .point-active {
+        background-color: aqua;
+    }
+    .point-current {
+        background-color: azure;
+    }
+
+</style>
---
+++ client/web/src/components/pages/game/controls/radar/Radar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <Point v-for='p in points'
+               :key='p.id'
+               :current="p.current"
+               :active="p.active"
+               :position="p.position">
+        </Point>
+    </div>
+</template>
+
+<script>
+    import Point from "@/components/pages/game/controls/radar/Point";
+
+    export default {
+        name: "Radar",
+        components: {Point},
+        props: {
+            angle_speed: Number,
+            range: Number,
+            map: Object,
+            root: Number,
+        },
+        computed: {
+            points() {
+                return [
+                    {
+                        id: 1,
+                        current: true,
+                        active: true,
+                        position: {
+                            x: 10,
+                            y: 10,
+                        }
+                    },
+                    {
+                        id: 2,
+                        current: false,
+                        active: true,
+                        position: {
+                            x: 20,
+                            y: 20,
+                        }
+                    },
+                    {
+                        id: 3,
+                        current: false,
+                        active: false,
+                        position: {
+                            x: 40,
+                            y: 40,
+                        }
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
