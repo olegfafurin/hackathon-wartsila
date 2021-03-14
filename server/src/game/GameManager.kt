722063c330c835474669a84a8cd2039394b0e408
amathael <dan.io.oreshnikov@gmail.com>
2021-03-14 15:09:38 +0300

    level 9

--- server/src/game/GameManager.kt
+++ server/src/game/GameManager.kt
@@ -11,7 +11,7 @@ object GameManager {
 
     fun createGame(username: String, roomName: String): Boolean {
         if (roomName in games.keys) return false
-        val newField = GameService.getFieldById(7) // TODO field generation
+        val newField = GameService.getFieldById(9) // TODO field generation
         val newGame = Game(newField, mutableListOf())
         games[roomName] = newGame
         addUserToGame(username, roomName)
