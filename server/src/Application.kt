38863428d99a8d046695173879e36f0cbd183fb6
amathael <dan.io.oreshnikov@gmail.com>
2021-03-14 06:12:26 +0300

    more trashb

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -73,7 +73,7 @@ fun Application.module(testing: Boolean = false) {
     })
 
     routing {
-        static("/game") {
+        static("/") {
             staticRootFolder = File("dist")
             files(".")
             default("index.html")
@@ -84,7 +84,7 @@ fun Application.module(testing: Boolean = false) {
 
         static("/assets") {
             staticRootFolder = File("assets")
-            static("images") { files("images") }
+            static("/images") { files("images") }
         }
 
         (UserRouting("user").install)()
