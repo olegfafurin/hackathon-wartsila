3997f16202d051d5bbd9cd7af95ced493847a858
amathael <dan.io.oreshnikov@gmail.com>
2021-03-14 07:05:35 +0300

    fix2

--- server/src/routing/GameRouting.kt
+++ server/src/routing/GameRouting.kt
@@ -20,28 +20,30 @@ class GameRouting(endpoint: String) : Routing(endpoint) {
     private val objectMapper = ObjectMapper()
 
     override val install: Route.() -> Unit = {
-        authenticate {
-            post("/create-room") {
-                val user = call.principal<User>()!!
-                val roomName = context.receive<String>()
-                gameManager.createGame(user, roomName)
-                val field = gameManager.getGameByUser(user)?.getKnownSubfield(user)!!
-                call.respond(field)
-            }
+        route("/$endpoint") {
+            authenticate {
+                post("/create-room") {
+                    val user = call.principal<User>()!!
+                    val roomName = context.receive<String>()
+                    gameManager.createGame(user, roomName)
+                    val field = gameManager.getGameByUser(user)?.getKnownSubfield(user)!!
+                    call.respond(field)
+                }
 
-            post("/find-room") {
-                val user = call.principal<User>()!!
-                val roomName = context.receive<String>()
-                gameManager.addUserToGame(user, roomName)
-                val field = gameManager.getGameByUser(user)?.getKnownSubfield(user)!!
-                call.respond(field)
-            }
+                post("/find-room") {
+                    val user = call.principal<User>()!!
+                    val roomName = context.receive<String>()
+                    gameManager.addUserToGame(user, roomName)
+                    val field = gameManager.getGameByUser(user)?.getKnownSubfield(user)!!
+                    call.respond(field)
+                }
 
-            get("/get-field") {
-                val user = call.principal<User>()!!
-                val game = gameManager.getGameByUser(user)
-                val field = game?.getKnownSubfield(user)
-                call.respond(field!!)
+                get("/get-field") {
+                    val user = call.principal<User>()!!
+                    val game = gameManager.getGameByUser(user)
+                    val field = game?.getKnownSubfield(user)
+                    call.respond(field!!)
+                }
             }
         }
     }
